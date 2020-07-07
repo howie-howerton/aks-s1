6e83010c25356d4fc88c8b88cefe40fe2c1c04f4
s1-howie <howieh@sentinelone.com>
2020-07-06 21:54:08 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,142 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "azurerm" {
+  #version = "=2.13.0" -> 2.17.0 (July 3rd, 2020)
+
+  features {}
+}
+
+provider "helm" {
+  #version = "1.2.2"
+  kubernetes {
+    host = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+
+    client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
+    load_config_file       = false
+  }
+}
+
+provider "kubernetes" {
+  load_config_file       = "false"
+  host                   = azurerm_kubernetes_cluster.cluster.kube_config.0.host
+  username               = azurerm_kubernetes_cluster.cluster.kube_config.0.username
+  password               = azurerm_kubernetes_cluster.cluster.kube_config.0.password
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name       = var.cluster_name
+  location   = azurerm_resource_group.rg.location
+  dns_prefix = var.dns_prefix
+
+  resource_group_name = azurerm_resource_group.rg.name
+  kubernetes_version  = "1.18.2"
+
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
+  }
+
+  default_node_pool {
+    name       = "nodepool"
+    node_count = var.agent_count
+    vm_size    = "Standard_D2s_v3" #"Standard_DS1_v2"
+  }
+
+  #   identity {
+  #     type = "SystemAssigned"
+  #   }
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+
+  tags = {
+    Environment = "aks-s1-demo"
+  }
+}
+# NEED TO CREATE A NAMESPACE AND A SECRET FOR S1 BEFORE RUNNING THE HELM PROVISIONER
+resource "kubernetes_namespace" "s1" {
+  metadata {
+    name = var.s1_namespace
+  }
+  depends_on = [azurerm_kubernetes_cluster.cluster]
+}
+
+locals {
+    dockerconfigjson = {
+        "auths" = {
+            (var.docker_server) = {
+                username = var.docker_username
+                password = var.docker_password
+                auth     = base64encode(join(":",[var.docker_username, var.docker_password]))
+            }
+        }
+    }
+}
+
+resource "kubernetes_secret" "s1" {
+  metadata {
+    name      = var.k8s_secret_for_s1_github_access_token
+    namespace = var.s1_namespace
+  }
+
+  data = {
+    ".dockerconfigjson" = jsonencode(local.dockerconfigjson)
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  depends_on = [kubernetes_namespace.s1, azurerm_kubernetes_cluster.cluster]
+}
+
+resource "helm_release" "local" {
+  name      = "s1"
+  chart     = "./cwpp_agent/helm_charts/sentinelone"
+  namespace = kubernetes_namespace.s1.metadata[0].name
+
+  set {
+    name  = "image.imagePullSecrets[0].name"
+    value = var.k8s_secret_for_s1_github_access_token
+  }
+  set {
+    name  = "helper.image.repository"
+    value = var.s1_helper_image_repository
+  }
+  set {
+    name  = "helper.image.tag"
+    value = var.s1_helper_image_tag
+  }
+  set {
+    name  = "helper.env.cluster"
+    value = var.cluster_name
+  }
+  set {
+    name  = "agent.image.repository"
+    value = var.s1_agent_image_repository
+  }
+  set {
+    name  = "agent.image.tag"
+    value = "ga-4.2.2"
+  }
+  set {
+    name  = "agent.env.site_key"
+    value = var.s1_site_key
+  }
+  depends_on = [kubernetes_namespace.s1, kubernetes_secret.s1]
+}
+
---
+++ vars.tf
@@ -0,0 +1,97 @@
+variable "client_id" {
+  description = "The client_id that K8s uses when creating Azure load balancers.  Can be the same as the client_id that terraform uses to create the cluster."
+}
+
+variable "client_secret" {
+  description = "The client_secret that K8s uses when creating Azure load balancers.  Can be the same as the client_secret that terraform uses to create the cluster."
+}
+
+variable "resource_group_name" {
+  description = "Name of the Resource Group to create."
+  default     = "aks-s1-rg"
+}
+variable "location" {
+  description = "The preferred Region into which to launch the resources outlined in this template."
+  default     = "eastus"
+}
+variable "cluster_name" {
+  description = "Cluster name for the AKS cluster"
+  default     = "aks-s1-demo"
+}
+
+variable "dns_prefix" {
+  description = "The DNS prefix that forms part of the FQDN used to access the cluster.  dns_prefix must contain between 2 and 45 characters. The name can contain only letters, numbers, and hyphens. The name must start with a letter and must end with an alphanumeric character."
+  default     = "aks-s1-demo"
+}
+
+variable "agent_count" {
+  description = "Number of worker nodes for the cluster."
+  default     = "2"
+}
+
+variable "ssh_public_key" {
+  description = "Path to the SSH public key to be used to log into the worker nodes (if necessary).."
+}
+
+
+variable "k8s_secret_for_s1_github_access_token" {
+  description = "This is the NAME of the K8s secret that stores your GitHub token (that is used to authenticate to the S1 GitHub packages repository (where the s1-agent and s1-helper Docker images are stored.))"
+  # ie: You need to get your gihub token via your account team (as of March 6th 2020)
+}
+
+variable "service_account_name" {
+  description = "Service Account Name to use"
+  default     = "sentinelone"
+}
+
+variable "s1_helper_image_repository" {
+  description = "The image/package repository where the s1_helper image is located."
+  default     = "docker.pkg.github.com/s1-agents/cwpp_agent/s1helper"
+}
+
+variable "s1_helper_image_tag" {
+  description = "Tag name (version) of the s1_helper image we want to use"
+  default     = "ga-4.2.2"
+}
+
+
+variable "s1_agent_image_repository" {
+  description = "The image/package repository where the s1_agent image is located."
+  default     = "docker.pkg.github.com/s1-agents/cwpp_agent/s1agent"
+}
+
+variable "s1_agent_image_tag" {
+  description = "Tag name (version) of the s1_agent image we want to use"
+  default     = "ga-4.2.2"
+}
+
+variable "s1_site_key" {
+  description = "The Sentinel One Site Key that the CWPP agent will use when communicating with the S1 portal."
+}
+
+variable "s1_namespace" {
+  description = "K8s namespace into which we will place the S1 cwpp agent(s)"
+  default     = "s1"
+}
+
+variable "docker_server" {
+  description = "Base Repo that houses S1 Docker images"
+  default     = "docker.pkg.github.com"
+}
+
+variable "docker_username" {
+  description = "Need more info as to why this is necessary.  It seems to be the username you use for Git when accessing GitHub." # ToDo:  Investigate why/if this is needed.
+}
+
+variable "docker_password" {
+  description = "GitHub access token to access the GitHub package/image repository (where the s1-agent and s1-helper images live)."
+}
+
+variable "docker_email" {
+  description = "Need more info as to why this is necessary.  It seems to be the email you use for Git when accessing GitHub." # ToDo:  Investigate why/if this is needed.
+}
+
+variable "helm_release_name" {
+  description = "Helm Releaes Name to use for the Helm deployment"
+  default     = "s1"
+}
